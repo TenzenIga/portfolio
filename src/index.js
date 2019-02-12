42746c3da3fbe1bd21f90996983b3844d64345ae
TenzenIga <arhan-93@list.ru>
2019-02-12 22:05:13 +0300

    added animations/ working on contact page

--- src/index.js
+++ src/index.js
@@ -2,18 +2,43 @@ import style from "./_scss/main.scss";
 
 const menuItems = document.querySelectorAll('.menu-item');
 
+const pages = document.querySelectorAll('.page')
 
-function toggleNavigation() {
-    const selectedPage = document.getElementById(this.dataset.name)
-    const activeSection =document.querySelector('.page-active')
-    const activePage = document.querySelector('.current')
-    activePage.classList.toggle('current')
-    activeSection.classList.toggle('page-active')
-    console.log(this.dataset.name);
-    
-    this.classList.toggle('current')
-    selectedPage.classList.toggle('page-active')
+const outClass = 'pt-page-moveToBottom';
+const	inClass = 'pt-page-scaleUp';
+let animationComplete = true;
+menuItems.forEach(item => item.addEventListener('click', changePage))
+
+pages.forEach(page=> page.addEventListener('animationend', removeAnimation));
+
+function changePage(){
+if(!animationComplete) return;
+animationComplete= false;
+const newPage= document.querySelector(`.${this.dataset.name}`)
+const prevPage = document.querySelector('.page_active');
+const current=document.getElementsByClassName('current')
+addAnimation(prevPage, newPage)
+current[0].classList.remove('current')
+this.classList.add('current')  
+
 }
 
-menuItems.forEach(item => item.addEventListener('click', toggleNavigation))
+function addAnimation(prevPage, newPage){
+  if(newPage.classList.contains('page_active')) return false;
+  prevPage.classList.add(outClass);
+  newPage.classList.toggle(inClass)
+  newPage.classList.add('page_active')   
+}
+function removeAnimation(e){
+    
+    if(e.animationName =='moveToBottom'){
+        this.classList.remove('page_active');
+        this.classList.remove(outClass);
+    }else{
+        this.classList.remove(inClass)
+        animationComplete = true;
+    }
+    
+}
+
 
